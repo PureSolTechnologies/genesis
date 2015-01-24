7b7d8743d88db00a7a3945cae80d4aa9817a0618
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-01-24 21:17:07 +0100

    Added IT tests for Cassandra tracker. Added module for filesystem tracker.

--- tracker/cassandra/src/main/java/com/puresoltechnologies/genesis/tracker/cassandra/CassandraTransformationTracker.java
+++ tracker/cassandra/src/main/java/com/puresoltechnologies/genesis/tracker/cassandra/CassandraTransformationTracker.java
@@ -1,8 +1,11 @@
 package com.puresoltechnologies.genesis.tracker.cassandra;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
+import java.net.URL;
 import java.util.Date;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,151 +34,176 @@ import com.puresoltechnologies.versioning.Version;
  */
 public class CassandraTransformationTracker implements TransformationTracker {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(CassandraTransformationTracker.class);
+    private static final Logger logger = LoggerFactory
+	    .getLogger(CassandraTransformationTracker.class);
 
-	public static final String KEYSPACE_NAME = "cluster_migration";
-	public static final String CHANGELOG_TABLE = "changelog";
+    public static final String DEFAULT_CASSANDRA_HOST_NAME = "localhost";
+    public static final int DEFAULT_CASSANDRA_PORT = 9042;
+    public static final String DEFAULT_KEYSPACE_NAME = "genesis";
+    public static final String CHANGELOG_TABLE = "changelog";
 
-	private static PreparedStatement preparedInsertStatement = null;
-	private static PreparedStatement preparedSelectStatement = null;
+    private PreparedStatement preparedInsertStatement = null;
+    private PreparedStatement preparedSelectStatement = null;
 
-	private Cluster cluster = null;
-	private Session session = null;
+    private Cluster cluster = null;
+    private Session session = null;
 
-	@Override
-	public void open() throws TransformationException {
-		connect();
-		prepareKeyspace();
+    private String host = DEFAULT_CASSANDRA_HOST_NAME;
+    private int port = DEFAULT_CASSANDRA_PORT;
+    private String keyspace = DEFAULT_KEYSPACE_NAME;
+
+    @Override
+    public void open() throws TransformationException {
+	loadAlternateConfigIfPresent();
+	connect();
+	prepareKeyspace();
+    }
+
+    private void loadAlternateConfigIfPresent() {
+	URL configuration = getClass().getResource(
+		"/genesis.tracker.cassandra.properties");
+	if (configuration != null) {
+	    try (InputStream stream = configuration.openStream()) {
+		Properties properties = new Properties();
+		properties.load(stream);
+		host = properties.getProperty("host",
+			DEFAULT_CASSANDRA_HOST_NAME);
+		keyspace = properties.getProperty("keyspace",
+			DEFAULT_KEYSPACE_NAME);
+		String portString = properties.getProperty("port",
+			Integer.toString(DEFAULT_CASSANDRA_PORT));
+		port = Integer.parseInt(portString);
+	    } catch (IOException e) {
+		System.err
+			.println("Warning: A configuration file for Cassandra Tracker was found, but could not be opened.");
+	    }
 	}
+    }
 
-	private void connect() {
-		if (cluster != null) {
-			throw new IllegalStateException(
-					"Cluster for migration tracker was already connected.");
-		}
-		cluster = CassandraUtils.connectCluster();
-		if (session != null) {
-			throw new IllegalStateException(
-					"Session for migration tracker was already opened.");
-		}
-		session = CassandraUtils.connectWithoutKeyspace(cluster);
+    private void connect() {
+	if (cluster != null) {
+	    throw new IllegalStateException(
+		    "Cluster for migration tracker was already connected.");
 	}
+	cluster = CassandraUtils.connectCluster(host, port);
+	if (session != null) {
+	    throw new IllegalStateException(
+		    "Session for migration tracker was already opened.");
+	}
+	session = CassandraUtils.connectWithoutKeyspace(cluster);
+    }
 
-	private void prepareKeyspace() throws TransformationException {
-		Metadata clusterMetadata = cluster.getMetadata();
-		KeyspaceMetadata keyspaceMetadata = clusterMetadata
-				.getKeyspace(CassandraTransformationTracker.KEYSPACE_NAME);
-		if (keyspaceMetadata == null) {
-			logger.info("Keyspace for Cassandra migration is missing. Needs to be created...");
-			session.execute("CREATE KEYSPACE "
-					+ CassandraTransformationTracker.KEYSPACE_NAME
-					+ " WITH replication " + "= {'class':'"
-					+ ReplicationStrategy.SIMPLE_STRATEGY.getStrategyName()
-					+ "', 'replication_factor':3};");
-			keyspaceMetadata = clusterMetadata
-					.getKeyspace(CassandraTransformationTracker.KEYSPACE_NAME);
-			if (keyspaceMetadata == null) {
-				throw new TransformationException("Could not create keyspace '"
-						+ CassandraTransformationTracker.KEYSPACE_NAME + "'.");
-			}
-			logger.info("Keyspace for Cassandra migration created.");
-		}
-		if (keyspaceMetadata
-				.getTable(CassandraTransformationTracker.CHANGELOG_TABLE) == null) {
-			logger.info("ChangeLog table for Cassandra migration is missing. Needs to be created...");
-			session.execute("CREATE TABLE "
-					+ CassandraTransformationTracker.KEYSPACE_NAME + "."
-					+ CassandraTransformationTracker.CHANGELOG_TABLE + " ("
-					+ "machine varchar, " + "component varchar, "
-					+ "time timestamp, " + "version varchar, "
-					+ "developer varchar, " + "command varchar, "
-					+ "hashid varchar, " + "comment varchar, "
-					+ "PRIMARY KEY(machine, component, version, command));");
-			logger.info("ChangeLog table for Cassandra migration created.");
-		}
+    private void prepareKeyspace() throws TransformationException {
+	Metadata clusterMetadata = cluster.getMetadata();
+	KeyspaceMetadata keyspaceMetadata = clusterMetadata
+		.getKeyspace(keyspace);
+	if (keyspaceMetadata == null) {
+	    logger.info("Keyspace for Cassandra migration is missing. Needs to be created...");
+	    session.execute("CREATE KEYSPACE " + keyspace
+		    + " WITH replication " + "= {'class':'"
+		    + ReplicationStrategy.SIMPLE_STRATEGY.getStrategyName()
+		    + "', 'replication_factor':3};");
+	    keyspaceMetadata = clusterMetadata.getKeyspace(keyspace);
+	    if (keyspaceMetadata == null) {
+		throw new TransformationException("Could not create keyspace '"
+			+ keyspace + "'.");
+	    }
+	    logger.info("Keyspace for Cassandra migration created.");
 	}
+	if (keyspaceMetadata
+		.getTable(CassandraTransformationTracker.CHANGELOG_TABLE) == null) {
+	    logger.info("ChangeLog table for Cassandra migration is missing. Needs to be created...");
+	    session.execute("CREATE TABLE " + keyspace + "."
+		    + CassandraTransformationTracker.CHANGELOG_TABLE + " ("
+		    + "machine varchar, " + "component varchar, "
+		    + "time timestamp, " + "version varchar, "
+		    + "developer varchar, " + "command varchar, "
+		    + "hashid varchar, " + "comment varchar, "
+		    + "PRIMARY KEY(machine, component, version, command));");
+	    logger.info("ChangeLog table for Cassandra migration created.");
+	}
+    }
 
-	private static synchronized void createPreparedStatements(Session session) {
-		if (preparedInsertStatement == null) {
-			preparedInsertStatement = session
-					.prepare("INSERT INTO "
-							+ KEYSPACE_NAME
-							+ "."
-							+ CHANGELOG_TABLE
-							+ " (time, version, developer, machine, component, command, hashid, comment)"
-							+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
-		}
-		if (preparedSelectStatement == null) {
-			preparedSelectStatement = session
-					.prepare("SELECT machine, version, component, command FROM "
-							+ KEYSPACE_NAME
-							+ "."
-							+ CHANGELOG_TABLE
-							+ " WHERE machine=? AND version=?"
-							+ " AND component=?" + " AND command=?" + ";");
-		}
+    private void createPreparedStatements(Session session) {
+	if (preparedInsertStatement == null) {
+	    preparedInsertStatement = session
+		    .prepare("INSERT INTO "
+			    + keyspace
+			    + "."
+			    + CHANGELOG_TABLE
+			    + " (time, version, developer, machine, component, command, hashid, comment)"
+			    + " VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
 	}
+	if (preparedSelectStatement == null) {
+	    preparedSelectStatement = session
+		    .prepare("SELECT machine, version, component, command FROM "
+			    + keyspace
+			    + "."
+			    + CHANGELOG_TABLE
+			    + " WHERE machine=? AND version=?"
+			    + " AND component=?" + " AND command=?" + ";");
+	}
+    }
 
-	@Override
-	public void close() {
-		if (session == null) {
-			throw new IllegalStateException(
-					"Session for migration tracker was not opened.");
-		}
-		session.close();
-		session = null;
-		if (cluster == null) {
-			throw new IllegalStateException(
-					"Cluster for migration tracker was not connected.");
-		}
-		cluster.close();
-		cluster = null;
+    @Override
+    public void close() {
+	if (session == null) {
+	    throw new IllegalStateException(
+		    "Session for migration tracker was not opened.");
 	}
+	session.close();
+	session = null;
+	if (cluster == null) {
+	    throw new IllegalStateException(
+		    "Cluster for migration tracker was not connected.");
+	}
+	cluster.close();
+	cluster = null;
+    }
 
-	@Override
-	public void trackMigration(String machine, TransformationMetadata metadata)
-			throws TransformationException {
-		if (preparedInsertStatement == null) {
-			createPreparedStatements(session);
-		}
-		try {
-			HashId hashId = HashUtilities.createHashId(metadata.getCommand());
-			BoundStatement boundStatement = preparedInsertStatement.bind(
-					new Date(), metadata.getTargetVersion().toString(),
-					metadata.getDeveloper(), machine,
-					metadata.getComponentName(), metadata.getCommand(),
-					hashId.toString(), metadata.getComment());
-			session.execute(boundStatement);
-		} catch (IOException e) {
-			throw new TransformationException(
-					"Could not track migration step.", e);
-		}
+    @Override
+    public void trackMigration(String machine, TransformationMetadata metadata)
+	    throws TransformationException {
+	if (preparedInsertStatement == null) {
+	    createPreparedStatements(session);
 	}
+	try {
+	    HashId hashId = HashUtilities.createHashId(metadata.getCommand());
+	    BoundStatement boundStatement = preparedInsertStatement.bind(
+		    new Date(), metadata.getTargetVersion().toString(),
+		    metadata.getDeveloper(), machine,
+		    metadata.getComponentName(), metadata.getCommand(),
+		    hashId.toString(), metadata.getComment());
+	    session.execute(boundStatement);
+	} catch (IOException e) {
+	    throw new TransformationException(
+		    "Could not track migration step.", e);
+	}
+    }
 
-	@Override
-	public boolean wasMigrated(String machine, String component,
-			Version version, String command) {
-		if (preparedSelectStatement == null) {
-			createPreparedStatements(session);
-		}
-		BoundStatement boundStatement = preparedSelectStatement.bind(machine,
-				version.toString(), component, command);
-		ResultSet result = session.execute(boundStatement);
-		return result.iterator().hasNext();
+    @Override
+    public boolean wasMigrated(String machine, String component,
+	    Version version, String command) {
+	if (preparedSelectStatement == null) {
+	    createPreparedStatements(session);
 	}
+	BoundStatement boundStatement = preparedSelectStatement.bind(machine,
+		version.toString(), component, command);
+	ResultSet result = session.execute(boundStatement);
+	return result.iterator().hasNext();
+    }
 
-	@Override
-	public void log(Date time,
-			com.puresoltechnologies.genesis.tracker.spi.Severity severity,
-			InetAddress machine, Thread thread, String message, Throwable cause) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void log(Date time,
+	    com.puresoltechnologies.genesis.tracker.spi.Severity severity,
+	    InetAddress machine, Thread thread, String message, Throwable cause) {
+	// TODO Auto-generated method stub
+    }
 
-	@Override
-	public TransformationMetadata getLastTransformationMetadata(String machine,
-			String component) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public TransformationMetadata getLastTransformationMetadata(String machine,
+	    String component) {
+	// TODO Auto-generated method stub
+	return null;
+    }
 }
---
+++ tracker/cassandra/src/test/java/com/puresoltechnologies/genesis/tracker/cassandra/CassandraTransformationTrackerIT.java
@@ -0,0 +1,62 @@
+package com.puresoltechnologies.genesis.tracker.cassandra;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.puresoltechnologies.genesis.commons.SequenceMetadata;
+import com.puresoltechnologies.genesis.commons.TransformationException;
+import com.puresoltechnologies.genesis.commons.TransformationMetadata;
+import com.puresoltechnologies.genesis.controller.GenesisController;
+import com.puresoltechnologies.genesis.controller.InvalidSequenceException;
+import com.puresoltechnologies.genesis.controller.NoTrackerFoundException;
+import com.puresoltechnologies.versioning.Version;
+import com.puresoltechnologies.versioning.VersionRange;
+
+public class CassandraTransformationTrackerIT {
+
+    /**
+     * Simple big bang test to check whether
+     * {@link CassandraTransformationTracker} can be loaded via SPI.
+     * 
+     * @throws NoTrackerFoundException
+     *             is thrown if no tracker was found.
+     * @throws TransformationException
+     *             if transformation fails.
+     * @throws InvalidSequenceException
+     *             if sequences is invalid.
+     */
+    @Test
+    public void testSPILoading() throws NoTrackerFoundException,
+	    TransformationException, InvalidSequenceException {
+	try (GenesisController controller = new GenesisController()) {
+	    controller.transform();
+	}
+    }
+
+    @Test
+    public void testTracking() throws TransformationException {
+	CassandraTransformationTracker tracker = new CassandraTransformationTracker();
+	tracker.open();
+	try {
+	    Version startVersion = new Version(1, 0, 0);
+	    Version targetVersion = new Version(2, 0, 0);
+	    Version nextVersion = new Version(3, 0, 0);
+	    VersionRange providedVersionRange = new VersionRange(targetVersion,
+		    true, nextVersion, false);
+	    String componentName = "testComponent1";
+	    SequenceMetadata sequenceMetadata = new SequenceMetadata(
+		    componentName, startVersion, providedVersionRange);
+	    String command = "command1";
+	    TransformationMetadata metadata = new TransformationMetadata(
+		    sequenceMetadata, "RRL", command, "comment1");
+	    String machine = "localmachine";
+	    tracker.trackMigration(machine, metadata);
+
+	    assertTrue(tracker.wasMigrated(machine, componentName,
+		    targetVersion, command));
+	} finally {
+	    tracker.close();
+	}
+    }
+}

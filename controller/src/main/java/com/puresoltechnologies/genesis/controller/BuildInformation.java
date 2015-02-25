8db21d2a0eb6dff1d0f47eb121d477143fbf303b
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-02-25 21:48:11 +0100

    #205 Version number information in Genesis is wrong

--- controller/src/main/java/com/puresoltechnologies/genesis/controller/BuildInformation.java
+++ controller/src/main/java/com/puresoltechnologies/genesis/controller/BuildInformation.java
@@ -2,27 +2,30 @@ package com.puresoltechnologies.genesis.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
-
-import com.puresoltechnologies.versioning.Version;
+import java.util.Properties;
 
 public class BuildInformation {
 
-	public static Version getVersion() {
-		try (InputStream manifestStream = BuildInformation.class
-				.getResourceAsStream("/META-INF/MANIFEST.MF")) {
-			Manifest manifest = new Manifest(manifestStream);
-			Attributes mainAttributes = manifest.getMainAttributes();
-			String versionString = mainAttributes
-					.getValue("Implementation-Version");
-			if (versionString == null) {
-				return new Version(0, 0, 0);
-			}
-			return Version.valueOf(versionString);
-		} catch (IOException e) {
-			return null;
-		}
+    private static final Properties buildProperties = new Properties();
+    static {
+	try (InputStream stream = BuildInformation.class
+		.getResourceAsStream("/META-INF/build.properties")) {
+	    buildProperties.load(stream);
+	} catch (IOException e) {
+	    throw new RuntimeException("Could not read build.properties.", e);
 	}
+    }
+
+    public static String getVersion() {
+	return buildProperties.get("project.version").toString();
+    }
+
+    public static String getInceptionYear() {
+	return buildProperties.get("project.inceptionYear").toString();
+    }
+
+    public static String getBuildYear() {
+	return buildProperties.get("project.buildYear").toString();
+    }
 
 }
